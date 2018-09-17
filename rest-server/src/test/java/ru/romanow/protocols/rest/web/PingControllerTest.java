6f93294735a1c5819597ea2491560d86c4d0b707
a.romanov <a.romanov@softpro.com>
2018-09-17 18:28:09 +0300

    rest docs

--- rest-server/src/test/java/ru/romanow/protocols/rest/web/PingControllerTest.java
+++ rest-server/src/test/java/ru/romanow/protocols/rest/web/PingControllerTest.java
@@ -3,16 +3,19 @@ package ru.romanow.protocols.rest.web;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.restdocs.AutoConfigureRestDocs;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.http.MediaType;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 
+import static org.springframework.restdocs.mockmvc.MockMvcRestDocumentation.document;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 @RunWith(SpringRunner.class)
 @WebMvcTest
+@AutoConfigureRestDocs
 public class PingControllerTest {
 
     @Autowired
@@ -23,7 +26,8 @@ public class PingControllerTest {
         mockMvc.perform(get("/api/ping"))
                 .andExpect(status().isOk())
                 .andExpect(content().contentType(MediaType.APPLICATION_JSON_UTF8))
-                .andExpect(jsonPath("$.response").value("ok"));
+                .andExpect(jsonPath("$.response").value("ok"))
+                .andDo(document("Ping"));
     }
 
     @Test
@@ -32,6 +36,7 @@ public class PingControllerTest {
                 .andExpect(status().isOk())
                 .andExpect(content().contentType(MediaType.APPLICATION_JSON_UTF8))
                 .andExpect(cookie().exists("TestCookie"))
-                .andExpect(jsonPath("$.response").value("ok"));
+                .andExpect(jsonPath("$.response").value("ok"))
+                .andDo(document("Cookies"));
     }
 }

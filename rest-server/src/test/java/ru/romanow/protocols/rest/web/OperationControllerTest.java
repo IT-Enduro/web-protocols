bde8c373675a42d5d32aba86a2935f34b0c4af94
a.romanov <a.romanov@softpro.com>
2018-09-18 13:04:45 +0300

    rest docs

--- rest-server/src/test/java/ru/romanow/protocols/rest/web/OperationControllerTest.java
+++ rest-server/src/test/java/ru/romanow/protocols/rest/web/OperationControllerTest.java
@@ -4,20 +4,25 @@ import com.google.gson.Gson;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.restdocs.AutoConfigureRestDocs;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.http.MediaType;
+import org.springframework.restdocs.payload.JsonFieldType;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 import ru.romanow.protocols.api.model.TestObjectRequest;
 
 import static org.apache.commons.lang3.RandomStringUtils.randomAlphabetic;
+import static org.springframework.restdocs.mockmvc.MockMvcRestDocumentation.document;
+import static org.springframework.restdocs.payload.PayloadDocumentation.fieldWithPath;
+import static org.springframework.restdocs.payload.PayloadDocumentation.requestFields;
+import static org.springframework.restdocs.payload.PayloadDocumentation.responseFields;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 @RunWith(SpringRunner.class)
 @WebMvcTest
+@AutoConfigureRestDocs
 public class OperationControllerTest {
     private static final Integer CODE = 100;
     private static final String NAME = "TEST";
@@ -40,7 +45,18 @@ public class OperationControllerTest {
                 .andExpect(status().isOk())
                 .andExpect(content().contentType(MediaType.APPLICATION_JSON_UTF8))
                 .andExpect(jsonPath("$.code").value(CODE))
-                .andExpect(jsonPath("$.data").value(NAME));
+                .andExpect(jsonPath("$.data").value(NAME))
+                .andDo(
+                        document("process",
+                                requestFields(
+                                        fieldWithPath("id").description("Request id").type(JsonFieldType.NUMBER),
+                                        fieldWithPath("searchString").description("Search string").type(JsonFieldType.STRING)
+                                ),
+                                responseFields(
+                                        fieldWithPath("code").description("Result code").type(JsonFieldType.NUMBER),
+                                        fieldWithPath("data").description("Payload").type(JsonFieldType.STRING)
+                                )
+                        ));
     }
 
     @Test
--- rest-server/src/test/java/ru/romanow/protocols/rest/web/PingControllerTest.java
+++ rest-server/src/test/java/ru/romanow/protocols/rest/web/PingControllerTest.java
@@ -6,10 +6,13 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.restdocs.AutoConfigureRestDocs;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.http.MediaType;
+import org.springframework.restdocs.payload.JsonFieldType;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 
 import static org.springframework.restdocs.mockmvc.MockMvcRestDocumentation.document;
+import static org.springframework.restdocs.payload.PayloadDocumentation.fieldWithPath;
+import static org.springframework.restdocs.payload.PayloadDocumentation.responseFields;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
@@ -27,7 +30,11 @@ public class PingControllerTest {
                 .andExpect(status().isOk())
                 .andExpect(content().contentType(MediaType.APPLICATION_JSON_UTF8))
                 .andExpect(jsonPath("$.response").value("ok"))
-                .andDo(document("Ping"));
+                .andDo(document("ping",
+                        responseFields(
+                                fieldWithPath("response").description("Available status").type(JsonFieldType.STRING)
+                        ))
+                );
     }
 
     @Test
@@ -37,6 +44,10 @@ public class PingControllerTest {
                 .andExpect(content().contentType(MediaType.APPLICATION_JSON_UTF8))
                 .andExpect(cookie().exists("TestCookie"))
                 .andExpect(jsonPath("$.response").value("ok"))
-                .andDo(document("Cookies"));
+                .andDo(document("cookies",
+                        responseFields(
+                                fieldWithPath("response").description("Available status").type(JsonFieldType.STRING)
+                        ))
+                );
     }
 }

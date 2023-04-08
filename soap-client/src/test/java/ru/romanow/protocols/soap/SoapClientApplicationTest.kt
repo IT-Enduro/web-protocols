d31fe7cc4307cbaa0e70e4f4e5766f60ff5d57d7
aromanov <aromanov@inno.tech>
2023-04-09 01:57:18 +0400

    add SOAP test

---
+++ soap-client/src/test/java/ru/romanow/protocols/soap/SoapClientApplicationTest.kt
@@ -0,0 +1,14 @@
+package ru.romanow.protocols.soap
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.test.context.ActiveProfiles
+
+@ActiveProfiles("test")
+@SpringBootTest
+class SoapClientApplicationTest {
+
+    @Test
+    fun test() {
+    }
+}
